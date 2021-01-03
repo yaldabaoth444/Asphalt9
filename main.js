044120e8eea39aa780143a4423a81e9485156698
yaldabaoth444 <25618671+yaldabaoth444@users.noreply.github.com>
2021-01-04 00:41:30 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const DEVICE = require('device.js');
 const PLAY = require('play.js').mp;
+const base = require('play.js').base;
 
 toast("The program will start running after 7 seconds, please quickly switch to the main interface of the game");
 sleep(3000);
@@ -10,26 +11,54 @@ DEVICE.checkPermission();
 DEVICE.setEventListener();
 DEVICE.savePower();
 
-var counterMP = 0;
+var startTime = new Date().getTime();
+var nowTime = new Date().getTime();
+var mp2Time = new Date().getTime();
 
+var counterMP = 0;
+const option1 = require('./profile.js').mp1;
+const option2 = require('./profile.js').mp2;
 
 for (;;counterMP++) {
 
+    //mp2
     if (1){
-        PLAY.beforeRun();
-  
-        if(PLAY.chooseCar()){
-            sleep(12000);
-            PLAY.run(counterMP);
-        }
-        // No oil
-        else{
-            //robot.back();
-            counterMP--;
-            sleep(500);
-            PLAY.goingHome();  
-            continue;
-        }
+        //nowTime = new Date().getTime();
+        PLAY.goingHome();
+        for ( let i = 0; i < 15; i++ ) {
+             
+            PLAY.beforeRun(option2);
+      
+            if(PLAY.chooseCar(option2)){
+                sleep(12000);
+                PLAY.run(counterMP);
+            }
+            // No oil
+            else{
+                base.back();
+                sleep(500);
+                break;
+            }
+        }    
+    }
+    if (0){
+        //nowTime = new Date().getTime();
+        PLAY.goingHome();
+        for ( let i = 0; i < 25; i++ ) {
+        
+            PLAY.beforeRun(option1);
+      
+            if(PLAY.chooseCar(option1)){
+                sleep(12000);
+                PLAY.run(counterMP);
+            }
+            // No oil
+            else{
+                base.back();
+                sleep(500);
+                break;
+            }
+        }    
     }
 }
 
