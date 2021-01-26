7218d4134c7293301a5a2f4a64de3b585d33af82
yaldabaoth444 <25618671+yaldabaoth444@users.noreply.github.com>
2021-01-27 00:46:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const DEVICE = require('device.js');
 const PLAY = require('play.js').mp;
+const HUNT = require('play.js').ch;
 const base = require('play.js').base;
 
 toast("The program will start running after 7 seconds, please quickly switch to the main interface of the game");
@@ -15,14 +16,31 @@ var startTime = new Date().getTime();
 var nowTime = new Date().getTime();
 var mp2Time = new Date().getTime();
 
-var counterMP = 0;
+var counter = { MP: 0, CH: 0 };
 const option1 = require('./profile.js').mp1;
 const option2 = require('./profile.js').mp2;
 
-for (;;counterMP++) {
-
+for (;;) {
+    //ch
+    if (1){
+        log("car hunt block begin");
+        //nowTime = new Date().getTime();
+        HUNT.goingHome();
+        for ( let i = 0; i < 4; i++ ) {
+             
+            HUNT.beforeRun(option2);
+            if(HUNT.chooseCar()){
+                sleep(5000);
+                HUNT.run(counter);
+            } else
+                break;
+        }
+        HUNT.goingHome();
+        log("car hunt block finish");    
+    }
     //mp2
     if (1){
+        log("multiplayer 2 block begin");
         //nowTime = new Date().getTime();
         PLAY.goingHome();
         for ( let i = 0; i < 15; i++ ) {
@@ -30,18 +48,19 @@ for (;;counterMP++) {
             PLAY.beforeRun(option2);
       
             if(PLAY.chooseCar(option2)){
-                sleep(12000);
-                PLAY.run(counterMP);
-            }
-            // No oil
-            else{
+                sleep(25000);
+                PLAY.run(counter);
+            } else {
+                // No oil
                 base.back();
                 sleep(500);
                 break;
             }
-        }    
+        }
+        log("multiplayer 2 block finish");    
     }
-    if (0){
+    if (1){
+        log("multiplayer 1 block begin");
         //nowTime = new Date().getTime();
         PLAY.goingHome();
         for ( let i = 0; i < 25; i++ ) {
@@ -49,17 +68,19 @@ for (;;counterMP++) {
             PLAY.beforeRun(option1);
       
             if(PLAY.chooseCar(option1)){
-                sleep(12000);
-                PLAY.run(counterMP);
-            }
-            // No oil
-            else{
+                sleep(25000);
+                PLAY.run(counter);
+            } else {
+                // No oil
                 base.back();
                 sleep(500);
                 break;
             }
-        }    
+        }
+        log("multiplayer 1 block finish");    
     }
+    log("timeout for 1 min");
+    sleep(60000);
 }
 
 toastLog("end script");   
